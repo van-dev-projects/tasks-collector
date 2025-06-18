ab17e4814cfc605111f70e6a714cb03320f40d1c
vani0-0 <van973vv@gmail.com>
2025-06-19 07:59:13 +0800

    First Commit

---
+++ src/config/site.ts
@@ -0,0 +1,19 @@
+export const siteConfig = {
+  name: "Tasks Collecter 💲",
+  url: "http://localhost:3000",
+  ogImage: "http://localhost:3000/og.jpg",
+  description:
+    "A Task saving program to organize workflows and ease of compiling past tasks",
+  links: {
+    twitter: "https://x.com/van_dev_1",
+    github: "https://github.com/van-dev-projects",
+  },
+  authors: [
+    {
+      name: "van",
+      url: "https://github.com/vani0-0",
+    },
+  ],
+};
+
+export type SiteConfig = typeof siteConfig;
